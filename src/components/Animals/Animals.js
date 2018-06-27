c268926305d43bfb0001cac6a469f12c570895e6
Xuehui Chen <jeffreychen2016@gmail.com>
2018-06-27 18:38:01 -0500

    added files for Animals and Form components

---
+++ src/components/Animals/Animals.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Animals.css';
+
+class Animals extends React.Component {
+  render () {
+    return (
+      <div className="Animals">Animals Div</div>
+    );
+  };
+};
+
+export default Animals;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Form.css';
+
+class Form extends React.Component {
+  render () {
+    return (
+      <div className="Form">
+        Form
+      </div>
+    );
+  };
+};
+
+export default Form;
